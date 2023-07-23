0c50ce06d5c592249ab4df6a87286353505ef95a
wpcodevo <ziddah.edem@gmail.com>
2023-07-23 07:32:06 +0000

    updated

--- .vercel/output/functions/fn.func/home/tmony/svelte/simple-app-in-sveltekit/.svelte-kit/output/server/chunks/prisma.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = globalThis.prisma ?? new PrismaClient({
-  log: ["query"]
-});
-if (process.env.NODE_ENV != "production")
-  globalThis.prisma = prisma;
-export {
-  prisma as p
-};
--- .vercel/output/functions/fn.func/home/tmony/svelte/simple-app-in-sveltekit/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.2b8ee5a5.js","_app/immutable/chunks/scheduler.4f524d2e.js","_app/immutable/chunks/index.096ecd25.js","_app/immutable/chunks/singletons.0cd6c329.js","_app/immutable/chunks/index.54d44800.js"];
-export const stylesheets = [];
-export const fonts = [];

5874ab7bbf97cae1ba56822c171777b61b1c9575
wpcodevo <ziddah.edem@gmail.com>
2023-07-24 11:38:13 +0000

    updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,7 +48,7 @@
 </main>
 {#if pageLoading}
 	<div
-		class="fixed top-2 left-2 sm:top-5 sm:left-5 inline-block h-4 w-4 sm:h-8 sm:w-8 animate-spin rounded-full border-4 border-solid border-yellow-400 border-r-transparent align-[-0.125em] text-warning motion-reduce:animate-[spin_1.5s_linear_infinite]"
+		class="fixed top-2 left-2 sm:top-5 sm:left-5 inline-block h-4 w-4 sm:h-8 sm:w-8 animate-spin rounded-full border-2 sm:border-4 border-solid border-yellow-400 border-r-transparent align-[-0.125em] text-warning motion-reduce:animate-[spin_1.5s_linear_infinite]"
 		role="status"
 	/>
 {/if}

cfa25af8e888cd2b19489b6a90a883f6fd20c521
wpcodevo <ziddah.edem@gmail.com>
2023-07-26 15:07:46 +0000

    updated

--- src/routes/api/hello/+server.ts
+++ src/routes/api/hello/+server.ts
@@ -1,14 +1,15 @@
-import { json } from "@sveltejs/kit";
+import { json } from '@sveltejs/kit';
 
-export async function GET() {
+export function GET() {
 	const responseBody = {
 		status: 'success',
 		message: 'Hello, SvelteKit'
 	};
-	return new Response(JSON.stringify(responseBody), {
-		status: 200,
-		headers: { 'Content-Type': 'application/json' }
-	});
 
-	// return json(responseBody)
+	return json(responseBody);
+
+	// return new Response(JSON.stringify(responseBody), {
+	// 	status: 200,
+	// 	headers: { 'Content-Type': 'application/json' }
+	// });
 }
